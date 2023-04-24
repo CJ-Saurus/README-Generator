553a94d797d36d47cb1ebece750a5d3dae4ec97a
Christopher <christopherperez@Christophers-Laptop.local>
2023-04-24 18:02:46 -0500

    gm

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license !== "none") {
+        return `![Github license](https://img.shields.io/badge/license-${license}-green.svg)`;
+    }
+    return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +18,30 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+${data.features}
+## Languages & Dependencies
+${data.require}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contributors}
+## Testing
+${data.test}
 `;
 }
 
